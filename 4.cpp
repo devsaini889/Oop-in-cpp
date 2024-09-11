6dda0eafb0ff4eaf1de688c101883e1af270ce91
devsaini889 <ds0752607@gmail.com>
2024-09-12 00:30:15 +0530

    Fourth

---
+++ 4.cpp
@@ -0,0 +1,28 @@
+//Function overloading
+
+#include<iostream>
+using namespace std;
+
+
+// Calculate the volume of a cylinder
+int volume(double r, int h){
+    return(3.14 * r *r *h);
+}
+
+// Calculate the volume of a cube
+int volume(int a){
+    return (a * a * a);
+}
+
+// Rectangular box
+int volume (int l, int b, int h){
+    return (l*b*h);
+}
+
+int main(){
+    
+    cout<<"The volume of cuboid of 3, 7 and 6 is "<<volume(3, 7, 6)<<endl;
+    cout<<"The volume of cylinder of radius 3 and height 6 is "<<volume(3, 6)<<endl;
+    cout<<"The volume of cube of side 3 is "<<volume(3)<<endl;
+    return 0;
+}
